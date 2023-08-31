bba5c336e85e7af7cf4cc3a31e249f4f1553d183
Subham-Beura <93039130+Subham-Beura@users.noreply.github.com>
2023-08-31 14:58:45 +0530

    Time: 79 ms (66.00%) | Memory: 13.3 MB (87.11%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -9,15 +9,11 @@ class Solution(object):
         for i in range(len(s)):
           l,r=i,i
           while l>=0 and r<len(s) and s[l]== s[r]:
-            # if s[l:r+1] not in allpall:
-            #   allpall.append(s[l:r+1])
             res+=1
             l-=1
             r+=1
           l,r=i,i+1 
           while l>=0 and r<len(s) and s[l]== s[r]:
-            # if s[l:r+1] not in allpall:
-            #   allpall.append(s[l:r+1])
             res+=1
             l-=1
             r+=1
