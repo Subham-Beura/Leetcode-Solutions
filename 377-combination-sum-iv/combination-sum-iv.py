47f6241da698274967129700cb0f112946cd85f0
Subham-Beura <93039130+Subham-Beura@users.noreply.github.com>
2023-09-18 21:31:03 +0530

    Time: 15 ms (90.21%) | Memory: 13 MB (98.94%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -5,17 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        dp=[-1]*(target+1)
-        def ans(nums,t):
-          if t==0:
-            return 1
-          if t<0: 
-            return 0
-          if dp[t] !=-1:
-            return dp[t]
-          res=0
-          for num in nums:
-             res+=ans(nums,t-num)
-          dp[t]=res
-          return res
-        return ans(nums,target)
+        dp=[0]*(target+1)
+        dp[0]=1
+        for i in range(target+1):
+          for n in nums:
+            if i-n>=0:
+              dp[i]+=dp[i-n]
+        return dp[target]
