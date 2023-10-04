79b5fd7d72f622b4b8ccd0167eeb1bddf7800e96
Subham-Beura <93039130+Subham-Beura@users.noreply.github.com>
2023-10-04 20:43:22 +0530

    Time: 527 ms (5.05%) | Memory: 24 MB (88.05%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        minv=9999999
+        for i in range(len(nums)):
+          if nums[i]<0:
+            nums[i]=0
+          elif nums[i]<minv and nums[i]>0:
+            minv=nums[i]
+        if minv>1:
+          return 1
+        
+        print(nums)
+        for i in range(len(nums)):
+          val=abs(nums[i])
+          if 1<=val<=len(nums):
+            if nums[val-1] >0:
+              nums[val-1]*=-1
+            if nums[val-1]==0:
+              nums[val-1]=(len(nums)+1)*-1
+        print(nums)
+        for i in range(1,len(nums)+1):
+          if nums[i-1]>=0:
+            return i
+        return len(nums)+1
