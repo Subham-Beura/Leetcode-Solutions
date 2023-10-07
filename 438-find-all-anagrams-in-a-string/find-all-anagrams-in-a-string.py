eaf4459de7e28aade852ff62ad52a04240049fd0
Subham-Beura <93039130+Subham-Beura@users.noreply.github.com>
2023-10-07 20:34:41 +0530

    Time: 690 ms (14.27%) | Memory: 14.5 MB (40.99%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        if len(s)<len(p):
+          return []
+        L=0
+        res=[]
+        pSum=0
+        pCounter={c:0 for c in s}
+        for c in p:
+          pSum+=ord(c)
+          pCounter[c]=1+pCounter.get(c,0)
+        windowSum=0
+        windowCounter={c:0 for c in s}
+        for c in range(len(p)-1):
+          windowSum+=ord(s[c])
+          windowCounter[s[c]]=1+windowCounter.get(s[c],0)
+        for R in range(len(p)-1,len(s)):
+          windowSum+=ord(s[R])
+          windowCounter[s[R]]=1+windowCounter.get(s[R],0)
+          if pSum==windowSum :
+            print(L)
+            print(windowCounter)
+            print(pCounter)
+            if windowCounter==pCounter:
+              res.append(L)
+          windowSum-=ord(s[L])
+          windowCounter[s[L]]-=1
+          L+=1
+        return res
+        
