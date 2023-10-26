e3e390ee1ef19409d4f0c5af963110132d873d82
Subham-Beura <93039130+Subham-Beura@users.noreply.github.com>
2023-10-26 20:13:53 +0530

    Time: 70 ms (79.23%) | Memory: 16.9 MB (35.40%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,19 +6,6 @@ class Solution(object):
         :rtype: List[int]
         """
         revCounter=[(v,key)for key,v in Counter(nums).items()]
-        heapq.heapify(revCounter)
-        
         res= heapq.nlargest(k,revCounter)
         return [key for val,key in res]
-        # if len(revCounter)==1:
-        #   return [revCounter[0][1]]
-        # print(revCounter)
-        # if k==1:
-        #   return [revCounter[-1][1]]
-        # for v,key in revCounter:
-        #   res.append(key)
-        # print(res)
-        # if k >= len(res):
-        #   return res
-        # return res[-k:]
         
