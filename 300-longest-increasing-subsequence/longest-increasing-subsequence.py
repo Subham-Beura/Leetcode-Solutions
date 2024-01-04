30bb98108d2ccd2e9bbc743400a6a480179ee673
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-04 18:06:53 +0530

    Time: 2121 ms (25.64%) | Memory: 13.6 MB (56.80%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -9,12 +9,12 @@ class Solution(object):
         
         if n==1 or n==1:
             return n
-
+        res=0
         for i in range(n):
             for j in range(0,i):
                 if nums[j]<nums[i]:
                     dp[i]=max(dp[i],dp[j]+1)
-        
-        return max(dp)
+            res=max(res,dp[i])        
+        return res
 
         
