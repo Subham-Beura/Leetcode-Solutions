8204f8f68dd5fa78e022c2f25639c720220b24d5
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-10 22:48:26 +0530

    Time: 405 ms (13.79%) | Memory: 64.8 MB (6.90%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -7,6 +7,7 @@ class Solution(object):
         cache={}
         def minOps(s,buffer):
             if (s,buffer) in cache:
+                print("hit")
                 return cache[(s,buffer)]
             i=len(s)
             if i==n:
@@ -14,8 +15,10 @@ class Solution(object):
                 return 0
             
             copyPaste,pasteAgain=1000,1000
+            # Paste current buffer again
             if len(s+buffer)<=n and buffer!="":
                 pasteAgain=minOps(s+buffer,buffer)+1
+            # Copy and Paste two step which cannot be interrupted
             if len(s+s)<=n:
                 copyPaste=minOps(s+s,s)+2
             cache[(s,buffer)]= min(pasteAgain,copyPaste)
