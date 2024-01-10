14dc2f224e242643be15be1fa398e20a78d8a4e2
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-10 19:34:53 +0530

    Time: 2579 ms (54.08%) | Memory: 102.8 MB (28.57%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.py
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -10,15 +10,17 @@ class Solution(object):
         def find(i,m,n):
             if (i,m,n) in cache:
                 return cache[(i,m,n)]
+            
             if i==len(strs):
                 return 0
+            
             w=strs[i]
             mc=w.count("0")
             nc=w.count("1")
 
             if m-mc<0 or n-nc<0:
                 cache[(i,m,n)]= find(i+1,m,n)
-            else:
-                cache[i,m,n]=max(find(i+1,m,n),find(i+1,m-mc,n-nc)+1)
+                return cache[i,m,n]
+            cache[i,m,n]=max(find(i+1,m,n),find(i+1,m-mc,n-nc)+1)
             return cache[i,m,n]
         return find(0,m,n)
