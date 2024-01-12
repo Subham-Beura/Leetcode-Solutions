fc6ae9744f472b9d5507708515733067a53c0035
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-12 19:03:28 +0530

    Time: 6067 ms (11.11%) | Memory: 215.9 MB (11.11%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.py
+++ 911-profitable-schemes/profitable-schemes.py
@@ -9,7 +9,6 @@ class Solution(object):
         """
         cache={}
         def noSchemes(i,no,score):
-            # print(str(i)+" "+str(no)+" "+str(score))
             if (i,no,score) in cache:
                 return cache[(i,no,score)]
             if no<0:
@@ -19,8 +18,8 @@ class Solution(object):
                 cache[(i,no,score)]= 1 if score>=minProfit else 0
                 return cache[(i,no,score)]
             doJob,noJob=0,0
-            if no-group[i+1]>=0:
-                doJob=noSchemes(i+1,no-group[i+1],min(minProfit,score+profit[i+1]))
+
+            doJob=noSchemes(i+1,no-group[i+1],min(minProfit,score+profit[i+1]))
             noJob=noSchemes(i+1,no,score)
 
             cache[(i,no,score)]= doJob+noJob
