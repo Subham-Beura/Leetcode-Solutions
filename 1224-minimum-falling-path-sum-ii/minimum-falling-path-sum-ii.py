486f0736233652cc9528125f23627796d18533c9
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-14 20:27:16 +0530

    Time: 3364 ms (11.76%) | Memory: 16.7 MB (64.71%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
@@ -8,15 +8,12 @@ class Solution(object):
         R=len(matrix)
         C=len(matrix[0])
         for i in range(R-2,-1,-1):
-            # print(matrix[i])
             nextRow=matrix[i+1]
             for j in range(C):
                 minv=99999999
-                for c,v in enumerate(nextRow):
+                for c in range(len(nextRow)):
                     if c==j:
                         continue
-                    minv=min(minv,v)
+                    minv=min(minv,nextRow[c])
                 matrix[i][j]+=minv
-                    
-        print(matrix)
         return min(matrix[0])
