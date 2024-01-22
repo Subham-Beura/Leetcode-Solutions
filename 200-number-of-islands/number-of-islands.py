1a6a4011f2e80084ae94703add97df5c135d235f
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-22 17:55:51 +0530

    Time: 210 ms (99.50%) | Memory: 18.9 MB (89.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,24 +2,21 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        discovered=[[False]*(n) for _ in range(m)]
         res=0
         def dfs(i,j):
             # Out of bounds
             if i<0 or j<0 or i>=m or j>=n:
-                return 
-            if discovered[i][j]:
-                return 
+                return  
             if grid[i][j]=="0":
                 return
-            discovered[i][j]=True
+            grid[i][j]="0"
             dfs(i+1,j)
             dfs(i-1,j)
             dfs(i,j-1)
             dfs(i,j+1)
         for r in range(m):
             for c in range(n):
-                if not discovered[r][c] and grid[r][c]=="1":
+                if grid[r][c]=="1":
                     dfs(r,c)
                     res+=1
         return res
