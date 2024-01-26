c6cc5d19e8b5cbceb6b69705a8a8af401269e209
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-01-26 19:17:58 +0530

    Time: 39 ms (80.73%) | Memory: 16.9 MB (59.86%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,30 +1,23 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        newNode=None
-        oldToNew={} 
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        graph={None:None}
+        if not node:
+            return None
+        if node.neighbors==[]:
+            return Node(node.val)
         def dfs(node):
-          if node in oldToNew:
-            return oldToNew[node]
-          copy=Node(node.val)
-          oldToNew[node]=copy
-          for n in node.neighbors:
-            copy.neighbors.append(dfs(n))
-          return copy
-
-        if node is None:
-          return node
-        newNode=dfs(node) 
-        print(newNode)
-        return newNode
+            if node not in graph:
+                graph[node]=Node(node.val)
+                for n in node.neighbors:
+                    graph[node].neighbors.append(dfs(n))
+            return graph[node]
+        return dfs(node)
