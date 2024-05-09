3dfd7ac6cd0b39a9b5e65a934b4e360cc364a6fe
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-09 21:10:29 +0530

    Time: 4 ms (61.36%) | Memory: 15.1 MB (15.96%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,34 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levels;
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if (root==NULL)
+        if (root == NULL)
             return {};
         vector<TreeNode*> currLevel;
         vector<TreeNode*> nextLevel;
         currLevel.push_back(root);
 
-            vector<int> nodeValueCurrLevel;
-        while(currLevel.size()!=0){
+        vector<int> nodeValueCurrLevel;
+        while (currLevel.size() != 0) {
             nodeValueCurrLevel={};
-            for(TreeNode* n:currLevel){
+            for (TreeNode* n : currLevel) {
                 nodeValueCurrLevel.push_back(n->val);
-                if(n->left)
+                if (n->left)
                     nextLevel.push_back(n->left);
-                if(n->right)
+                if (n->right)
                     nextLevel.push_back(n->right);
             }
             levels.push_back(nodeValueCurrLevel);
-            currLevel=nextLevel;
-            nextLevel={};
+            currLevel = nextLevel;
+            nextLevel = {};
         }
         return levels;
-
     }
 };
