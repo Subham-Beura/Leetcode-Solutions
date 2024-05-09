5f2013e388dd918d390568f13842682eac1fb815
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-09 14:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (98.19%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -14,31 +14,20 @@ class Solution {
 public:
     vector<int> leaf1;
     vector<int> leaf2;
-    void getLeafsLeftToRight(TreeNode* root) {
+    void getLeafsLeftToRight(TreeNode* root,vector<int>&leaf) {
         if(root==NULL){
             return;
         }
         if (root->left == NULL && root->right == NULL) {
-            leaf1.push_back(root->val);
+            leaf.push_back(root->val);
             return;
         }
-        getLeafsLeftToRight(root->left);
-        getLeafsLeftToRight(root->right);
-    }
-    void getLeafsLeftToRight2(TreeNode* root) {
-        if(root==NULL){
-            return;
-        }
-        if (root->left == NULL && root->right == NULL) {
-            leaf2.push_back(root->val);
-            return;
-        }
-        getLeafsLeftToRight2(root->left);
-        getLeafsLeftToRight2(root->right);
+        getLeafsLeftToRight(root->left,leaf);
+        getLeafsLeftToRight(root->right,leaf);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        getLeafsLeftToRight(root1);
-        getLeafsLeftToRight2(root2);
+        getLeafsLeftToRight(root1,leaf1);
+        getLeafsLeftToRight(root2,leaf2);
         if (leaf1.size() != leaf2.size()) {
             return false;
         }
