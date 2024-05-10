3aa647b46a1fa85d0be1f598b1efbca0e87f6239
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-10 20:51:37 +0530

    Time: 81 ms (48.64%) | Memory: 12.9 MB (44.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,11 +4,9 @@ public:
         vector<int> dp(nums.size(), 1);
         int res = 1;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+            for (int j = 0; j < i; j++) 
+                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) 
                     dp[i] = dp[j] + 1;
-                }
-            }
             res = max(dp[i], res);
         }
         return res;
