accc2c59242646d6a15ed6444364ff3a55b540e1
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-11 18:34:53 +0530

    Time: 176 ms (6.70%) | Memory: 17.5 MB (8.48%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -1,24 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def flatten(self, root):
-        """
-        :type root: TreeNode
-        :rtype: None Do not return anything, modify root in-place instead.
-        """
-        def dfs(root):
-          if not root:
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if not root:
             return None
-          leftTail=dfs(root.left)
-          rightTail=dfs(root.right)
-
-          if leftTail:
-            leftTail.right=root.right
-            root.right=root.left
+        
+        self.flattenHelper(root)
+        print(root)
+        return root
+    def flattenHelper(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if  root.left is None and  root.right is None:
+            return [root,root]
+        if root.left and not root.right:
+            left=self.flattenHelper(root.left)
+            root.right=left[0]
             root.left=None
-          return rightTail or leftTail or root
-        dfs(root)
+            return [root,left[1]]
+        if not root.left and root.right:
+            return [root,self.flattenHelper(root.right)[1]]
+        left=self.flattenHelper(root.left)
+        right=self.flattenHelper(root.right)
+
+        root.right=left[0]
+        left[1].right=right[0]
+        root.left=None
+        return [root,right[1]] 
+        
