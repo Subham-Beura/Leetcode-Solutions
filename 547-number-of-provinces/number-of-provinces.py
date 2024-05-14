3d4a1746840a8bd95bbb1bfdc4edb70d479632a7
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-14 19:56:11 +0530

    Time: 188 ms (41.29%) | Memory: 17.3 MB (61.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -2,7 +2,7 @@ class Solution:
     visited=set()
     def exploreAll(self,V,adj):
         self.visited.add(V)
-        for i in range(len(adj[0])):
+        for i in range(len(adj)):
             if adj[V][i]==1 and i not in self.visited:
                 self.exploreAll(i,adj)
     def findCircleNum(self, adj: List[List[int]]) -> int:
