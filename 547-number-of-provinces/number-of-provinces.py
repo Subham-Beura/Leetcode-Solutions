289120883cd3d8c489a85321457d206914b1565d
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-14 19:58:40 +0530

    Time: 182 ms (68.41%) | Memory: 17.3 MB (61.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,7 +1,16 @@
-from scipy.sparse.csgraph import connected_components as cc
-from numpy import array as arr
-
-
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        return cc(arr(isConnected))[0]
+    visited=set()
+    def exploreAll(self,V,adj):
+        self.visited.add(V)
+        for i in range(len(adj)):
+            if adj[V][i]==1 and i not in self.visited:
+                self.exploreAll(i,adj)
+    def findCircleNum(self, adj: List[List[int]]) -> int:
+        res=0
+        self.visited=set()
+        n=len(adj)
+        for i in range(n):
+            if i not in self.visited:
+                res+=1
+                self.exploreAll(i,adj)
+        return res
