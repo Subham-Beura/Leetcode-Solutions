940d8f627ef093dbe452a1364076827b9ed3cd26
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-29 02:47:25 +0530

    Time: 1775 ms (8.07%) | Memory: 32.8 MB (42.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,17 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp={}
-        @cache
+        
         def noOfWays(value,minCoin):
             if value==0 or value==minCoin:
                 return 1
             if value<0 or value<minCoin:
                 return 0
+            if (value,minCoin) in dp:
+                return dp[(value,minCoin)]
             res=0
             for c in coins:
                 if c<minCoin:
                     continue
                 res+=noOfWays(value-c,c)
+            dp[(value,minCoin)]=res
             return res
         return noOfWays(amount,0)
         
