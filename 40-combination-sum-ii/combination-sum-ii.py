3233640d8e8af950446ee8364e6fc1b7d7ae98ed
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-05-29 19:02:48 +0530

    Time: 85 ms (9.98%) | Memory: 16.5 MB (76.27%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,28 +1,19 @@
-class Solution(object):
-    def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
+        n=len(candidates)
         candidates.sort()
-        def dfs(i,cur,total):
-          if total==target:
-            cur.sort()
-            res.append(cur) 
-            return 
-          if total >=target or i>=len(candidates):
-            return 
-          
-          dfs(i+1,cur+[candidates[i]],total+candidates[i])
-          old=candidates[i]
-          while(i+1<=len(candidates) and candidates[i]==old ):
-            i+=1
-          dfs(i,cur,total)
-        
+        def dfs(i,path,currSum):
+            if  currSum==target:
+                res.append(path)
+                return
+            if i>=n or  currSum>target:
+                return
+            dfs(i+1,path+[candidates[i]],currSum+candidates[i])
+            old=candidates[i]
+            while(i+1<=len(candidates) and candidates[i]==old ):
+                i+=1
+            dfs(i,path,currSum)
+
         dfs(0,[],0)
-
-
-        # return s
         return res
