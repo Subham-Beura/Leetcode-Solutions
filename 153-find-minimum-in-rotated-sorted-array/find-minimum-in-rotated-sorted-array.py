71b51dfa3cba088ad22cd6418e38fddfcd5e3161
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-06-03 19:37:26 +0530

    Time: 33 ms (96.74%) | Memory: 16.8 MB (95.14%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -3,15 +3,13 @@ class Solution:
         L,R=0,len(nums)-1
         while L<=R:
             M=(L+R)//2
-            print(f"{L} {M} {R}")
             l=nums[L] 
             r=nums[R]
             m=nums[M]
 
             if l<=r:
                 return nums[L]
-            
-            if M!=0 and nums[M-1]>m: 
+            if nums[M-1]>m: 
                 return nums[M] 
             if m<=r:
                 R=M-1
