3f82b4fa456c9e55b11c5f0173fce2366f36498b
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-06-11 07:35:16 +0530

    Time: 45 ms (33.29%) | Memory: 16.7 MB (67.06%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+
+        counter=Counter(arr1)
+        res=[0]*(len(arr1))
+        count=0
+        for n in arr2:
+            for c in range(counter[n]):
+                res[count]=n
+                count+=1
+            counter[n]=0
+        counter=dict(sorted(counter.items()))
+        for k,v in counter.items():
+            if v==0:
+                continue
+            for c in range(v):
+                 res[count]=k
+                 count+=1
+        return res 
+        
