6fb51c01c0654d6145b0dbd426467a41d63a7f81
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-06-25 07:03:47 +0530

    Time: 25 ms (97.80%) | Memory: 16.5 MB (74.62%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -6,32 +6,15 @@
 #         self.right = right
 class Solution:
     inord=[]
+    c=0
     def inorder(self,root):
         if not root:
             return 
-        self.inorder(root.left)
-        self.inord.append(root.val)
         self.inorder(root.right)
-
+        self.c+=root.val
+        root.val=self.c
+        self.inorder(root.left)
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        self.inord=[]
         self.inorder(root)
-        gt=self.inord
-        map=Counter(self.inord)
-        add=gt[-1]
-        map[add]=add
-        for i in range(len(gt)-2,-1,-1):
-            res=gt[i]+add 
-            map[gt[i]]=res
-            add=res
-
-        def dfs(root):
-            if not root:
-                return
-            root.val=map[root.val]
-            dfs(root.left)
-            dfs(root.right)
-            return root
-        
-        return dfs(root)
+        return root
         
