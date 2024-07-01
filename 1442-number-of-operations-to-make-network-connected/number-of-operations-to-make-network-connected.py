9b0957b336b65cbdf406da6e1369e90c7e90fa3e
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-01 16:34:04 +0530

    Time: 381 ms (72.34%) | Memory: 36.4 MB (67.90%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,35 +1,32 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        parent=[i for i in range(n)]
-        rank=[1]*(n)
+        parent = [i for i in range(n)]
+
         def find(x):
-            p=parent[x]
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
 
-            while p!=parent[p]:
-                parent[p]=parent[parent[p]]
-                p=parent[p]
-            parent[x]=p
-            return p
-        def union(n1,n2):
-            p1,p2=find(n1),find(n2)
-            
-            if p1==p2:return False
-
-            if rank[p1]>rank[p2]:
-                parent[p2]=p1
-                rank[p1]+=rank[p2]
-            else:
-                parent[p1]=p2
-                rank[p2]+=rank[p1]
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return False
+            parent[p2] = p1
             return True
-        uneededEdges=0
-        for n1,n2 in connections:
-            u=union(n1,n2)
-            if not u :
-                uneededEdges+=1
-        noOfCluster=len(set([find(i) for i in parent])) 
-        minEdgesForFullConnection=noOfCluster-1
-        if uneededEdges<minEdgesForFullConnection:
-            return -1
         
-        return noOfCluster-1
+        # Strategy:
+        # Count uneeded Edges
+        #   where n1 and n2 are in same cluster
+        # Count No of clusters
+        # Check if we have enough to conn cluster
+        uneededEdges = 0
+        for n1, n2 in connections:
+            if not union(n1, n2):
+                uneededEdges += 1
+
+        noOfCluster = len(set([find(i) for i in parent]))
+        minEdgesForFullConnection = noOfCluster - 1
+        if uneededEdges < minEdgesForFullConnection:
+            return -1
+
+        return noOfCluster - 1
