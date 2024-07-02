e2ca8e3a3c89183c02bef209b9f070a80279e1f7
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-02 20:36:37 +0530

    Time: 314 ms (37.46%) | Memory: 25.9 MB (38.66%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,14 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        dictionary.sort()
+        sentence=sentence.split(" ")
+        res=""
+        print(sentence)
+        for i,word in enumerate(sentence):
+            for root in dictionary:
+                if root in word[:len(root)]:
+                    sentence[i]=root
+                    break
+            res+=sentence[i]+" "
+        return res[:-1]
+        
