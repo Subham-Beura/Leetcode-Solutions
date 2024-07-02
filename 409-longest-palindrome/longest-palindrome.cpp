e7d8c459dd9d67584c964d37d52cd9b367aea7b3
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-02 17:58:15 +0530

    Time: 3 ms (61.09%) | Memory: 9 MB (35.61%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int> map;
+        unordered_map<char, int> map;
         for (char c : s)
             map[c]++;
-        int sum=0;
-        int maxOdd=0;
-        for(auto it=map.begin();it!=map.end();it++){
-            if((it->second&1)==1){
-                sum+=it->second-1 ;
-                maxOdd=1;
+        int sum = 0;
+        int maxOdd = 0;
+        for (auto it = map.begin(); it != map.end(); it++) {
+            if ((it->second & 1) == 0) {
+                sum += it->second;
                 continue;
             }
-            sum+=it->second;
+            sum += it->second - 1;
+            maxOdd = 1;
+            continue;
         }
-        return (maxOdd)?sum+1:sum;
+        return (maxOdd) ? sum + 1 : sum;
     }
 };
