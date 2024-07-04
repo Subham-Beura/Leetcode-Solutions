0b061816c43a13f2ac067944ab21767e8a36649d
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-04 16:36:44 +0530

    Time: 32 ms (94.75%) | Memory: 16.5 MB (98.27%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,14 @@
 class Solution:
-    
+
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def helper(nums):
-            if len(nums)==1:
-                return [nums]
-            if len(nums)==2:
-                return [[nums[0],nums[1]],[nums[1],nums[0]]]
-            res=[]
-            prev=helper(nums[:-1])
-            for p in prev:
-                for i in range(len(p)+1):
-                    newP=p[:i]+[nums[-1]]+p[i:]
-                    res.append(newP) 
-            return res 
-        return helper(nums)
+        if len(nums) == 1:
+            return [nums]
+        if len(nums) == 2:
+            return [[nums[0], nums[1]], [nums[1], nums[0]]]
+        res = []
+        prev = self.permute(nums[:-1])
+        for p in prev:
+            for i in range(len(p) + 1):
+                newP = p[:i] + [nums[-1]] + p[i:]
+                res.append(newP)
+        return res
