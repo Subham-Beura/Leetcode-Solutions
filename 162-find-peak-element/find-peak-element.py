3fc1ec9d8d3cd30481bb609262c7dfd006d688cb
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-05 18:06:41 +0530

    Time: 40 ms (90.87%) | Memory: 16.6 MB (93.42%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -7,8 +7,12 @@ class Solution:
         L,R=0,len(nums)-1
         while L<=R:
             mid=(L+R)//2
-            if nums[mid-1]>nums[mid]:
+            if (mid==0 and nums[1]<nums[0]) or (mid==len(nums)-1 and nums[-2]<nums[-1]):
+                return mid
+            if nums[mid-1]<nums[mid] and nums[mid]>nums[mid+1]:
+                return mid
+            elif nums[mid-1]>nums[mid]:
                 R=mid-1
             else:
                 L=mid+1
-        return L-1
+        return -1
