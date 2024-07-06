15f43e208fe2ab2c33560650fc7e3871665c8052
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-06 18:18:02 +0530

    Time: 44 ms (38.03%) | Memory: 17.1 MB (5.77%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,7 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        if len(citations)<=1:
-            return 1 if citations[0]>0 else 0
+        # if len(citations)<=1:
+        #     return 1 if citations[0]>0 else 0
         citations.append(float("inf"))
         citations.sort(key=lambda x:-1*x)
         # print(citations)
