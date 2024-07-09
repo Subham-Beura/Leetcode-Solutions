e6c418a277d097c3b14cfec889c30db8a79f9c4a
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-09 17:19:34 +0530

    Time: 742 ms (71.82%) | Memory: 57.2 MB (9.97%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,17 @@
+class Solution:
+    def averageWaitingTime(self, customer: List[List[int]]) -> float:
+        n=len(customer)
+        currentTime=customer[0][0]
+        busyTill=currentTime+customer[0][1]
+        total=customer[0][1]
+        for i in range(1,n):
+            entryTime=customer[i][0]
+            timeTaken=customer[i][1]
+
+            if entryTime>busyTill:
+                total+=timeTaken
+                busyTill=entryTime+timeTaken
+            else:
+                busyTill+=timeTaken
+                total+=busyTill-entryTime
+        return total/n
