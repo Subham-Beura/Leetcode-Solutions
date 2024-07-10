c7a7d868156811e6be4c5e78d0ef70485d4a2a60
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-10 20:57:33 +0530

    Time: 163 ms (88.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select s.machine_id,round(avg(cast(e.timestamp -s.timestamp as decimal)),3) as processing_time
+from activity s
+inner join activity e
+on s.machine_id=e.machine_id and s.process_id =e.process_id
+and s.activity_type= 'start' 
+and e.activity_type='end' and e.timestamp is not null
+group by s.machine_id
