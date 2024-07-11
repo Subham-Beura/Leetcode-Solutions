c2e17167ae219b060e98ddd1a05723ad6bb3f347
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-11 17:34:15 +0530

    Time: 40 ms (25.45%) | Memory: 16.6 MB (40.89%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -6,12 +6,13 @@ class Solution:
                 stack.append(c)
                 continue
             i=len(stack)-1
-            q=[]
+            q=deque()
             while stack and i>=0 and stack[i]!='(':
                 k=stack.pop()
                 q.append(k)  
                 i-=1
             stack.pop()
-            stack.extend(q)
+            for k in q:
+                stack.append(k)
             # print(stack)
         return ''.join(str(e) for e in stack)
