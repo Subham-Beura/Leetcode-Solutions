342ceab07296b73d411f36a8a3fcb2cea86ffee9
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-18 17:43:22 +0530

    Time: 142 ms (34.55%) | Memory: 17 MB (22.83%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
@@ -7,38 +7,33 @@
 class Solution:
     distToLeaf={}
     count=0
-    d=0
-    def getDistToLeaf(self,root):
+    def getDistToLeaf(self,root,d):
         if not root:
             return
         if not root.left and not root.right:
             self.distToLeaf[root]=[0]
             return
-        self.getDistToLeaf(root.left)
-        self.getDistToLeaf(root.right)
+        self.getDistToLeaf(root.left,d)
+        self.getDistToLeaf(root.right,d)
         
         leftLeafs=self.distToLeaf[root.left][::] if root.left else []
         rightLeafs=self.distToLeaf[root.right][::] if root.right else []
         
         leftLeafs=[l+1 for l in leftLeafs]
         rightLeafs=[r+1 for r in rightLeafs]
-
         self.distToLeaf[root]=rightLeafs+leftLeafs
+
         if not leftLeafs or not rightLeafs:
             return 
-        # print(root.val)
+
         for l in leftLeafs:
             for r in rightLeafs:
-                # print(f"{l} {r}")
-                if l+r<=self.d:
+                if l+r<=d:
                     
                     self.count+=1
         return 
     def countPairs(self, root: TreeNode, distance: int) -> int:
         self.distToLeaf={}
-        self.d=distance
-        self.getDistToLeaf(root)
-        distToLeaf=self.distToLeaf
-        # for k in self.distToLeaf:
-        #     print(f"{k.val} : {self.distToLeaf[k]}")
+        self.getDistToLeaf(root,distance)
+
         return self.count
