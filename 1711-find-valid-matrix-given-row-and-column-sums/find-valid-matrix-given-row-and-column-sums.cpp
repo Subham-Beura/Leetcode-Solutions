547cca5860f176ca6087c99232d0156fba826324
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-20 17:17:09 +0530

    Time: 40 ms (38.83%) | Memory: 36.1 MB (16.25%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,15 +1,18 @@
- class Solution {
+class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum,
                                       vector<int>& colSum) {
 
-        vector<vector<int>> res(rowSum.size(),vector<int>(colSum.size(),0));
-        for( int r=0;r<rowSum.size();r++)
-            for(int c=0;c<colSum.size();c++){
-                res[r][c]=min(rowSum[r],colSum[c]);
-                rowSum[r]-=res[r][c];
-                colSum[c]-=res[r][c];
+        vector<vector<int>> res(rowSum.size(), vector<int>(colSum.size(), 0));
+        for (int r = 0; r < rowSum.size(); r++) {
+            for (int c = 0; c < colSum.size(); c++) {
+                res[r][c] = min(rowSum[r], colSum[c]);
+                rowSum[r] -= res[r][c];
+                colSum[c] -= res[r][c];
+                if (rowSum[r] == 0)
+                    break;
             }
+        }
         return res;
     }
 };
