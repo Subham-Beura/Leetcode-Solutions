dc0a7797345f7f04fd2e1cb65d553f51c6ac4dc1
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-20 18:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (54.57%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    int findNumber(string s, int i, int n) {
-        if (i == n) {
-            return 1;
-        }
-        if (s[i] == '0') {
-            return 0;
-        }
-        int count = 0;
-        if (i != n - 1 && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
-            count += findNumber(s, i + 2, n);
-        }
-        count += findNumber(s, i + 1, n);
-        return count;
-    }
     int numDecodings(string s) {
         vector<int> cache(s.length() + 1, 0);
         cache[s.length()] = 1;
