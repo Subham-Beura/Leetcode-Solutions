07cfdef9f54457b6f7eab27c2bdddd9e58390fe1
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-07-22 19:09:28 +0530

    Time: 98 ms (22.58%) | Memory: 18.4 MB (53.75%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # Set up preq graph
-        preqs={i:[] for i in range(numCourses)}
+        if not prerequisites:
+            return range(numCourses)
+        G={i:[] for i in range(numCourses)}
+        preqs=[0 for _ in range(numCourses)]
         for c,p in prerequisites:
-            preqs[c].append(p)
-        visited=set()
-        res=[]
-        def dfs(c):
-            if c in visited:
-                return False
-            if preqs[c]==[]:
-                return True
-            
-            visited.add(c)
-            for p in preqs[c]:
-                if not dfs(p):
-                    return False
-            res.append(c)
-            visited.remove(c)
-            preqs[c]=[]
-            return True
-        for c in range(numCourses):
-            if preqs[c]==[]:
-                res.append(c)
-        for c in range(numCourses):
-                if not dfs(c):
-                    return []
-        return res
+            preqs[c]+=1
+            G[p].append(c)
+        Q=deque()
+        
+        for c,nP in enumerate(preqs):
+            # print(f"{c} {nP}")
+            if nP==0:
+                Q.append(c)
+        # print(G)
+        # print(preqs)
+        # print(Q)
+        topo=[]
+        while Q:
+            # print(Q)
+            node=Q.popleft()
+            topo.append(node)
+            for n in G[node]:
+                preqs[n]-=1
+                if preqs[n]==0:
+                    Q.append(n)
+        return topo if len(topo)==numCourses else []
 
         
