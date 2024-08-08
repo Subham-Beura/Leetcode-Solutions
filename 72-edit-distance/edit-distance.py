082244f5160f7ff3a211ab27bdc2a951f8dc843b
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-08-08 22:43:00 +0530

    Time: 105 ms (48.66%) | Memory: 20.1 MB (57.91%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,32 +1,26 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         word1=" "+word1
         word2=" "+word2
         n=len(word1)
         m=len(word2)
-
+        if n==0 or m==0:
+            return max(n,m)
         dp=[[0]*(m) for _ in range(n)]
-
         for r in range(n):
             dp[r][0]=r
+
         for c in range(m):
             dp[0][c]=c
-        
-
-        for i in range(1,n):
-            for j in range(1,m):
-                if word1[i]==word2[j]:
-                    dp[i][j]=dp[i-1][j-1]
-                else:
-                    insert=dp[i][j-1]+1
-                    dele=dp[i-1][j]+1
-                    replace=dp[i-1][j-1]+1
-                    dp[i][j]=min(insert,dele,replace)
-        
+        # print(dp)
+        for r in range(1,n):
+            for c in range(1,m):
+                if word1[r]==word2[c]:
+                    dp[r][c]=dp[r-1][c-1]
+                    continue
+                replace=dp[r-1][c-1]
+                add=dp[r-1][c]
+                remove=dp[r][c-1]
+                dp[r][c]=min(replace,add,remove)+1
         return dp[n-1][m-1]
-        
+
