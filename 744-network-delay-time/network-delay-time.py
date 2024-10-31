b1f998b2035af0a75dc87afe2d414f2a18553e9f
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-01 00:49:26 +0530

    Time: 354 ms (96.02%) | Memory: 18.6 MB (40.86%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -5,16 +5,23 @@ class Solution:
             G[s].append((d,w))
         
         visited=set()
+        # Min Heap to store the Edges and get cheapest possible edges every time
+        # Use Format (total_cost,edge_name)
         minHeap=[(0,k)]
         maxTime=0
 
         while minHeap:
+            # Get cheapest edge to expand
             weight,node=heapq.heappop(minHeap)
             
+            # Skip if already visited
             if node in visited:
                 continue
+            
+            # Add to visited list to prevent cycles
             visited.add(node)
 
+            # Check if dist from src to this node is max Distance. DJA gives 
             maxTime=max(maxTime,weight)
 
             for nei,w in G[node]:
