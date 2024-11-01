faede6983567a6cecb1be63c09b4c4820ab24912
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-01 22:38:12 +0530

    Time: 90 ms (84.98%) | Memory: 27.6 MB (96.10%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -4,13 +4,13 @@ class Solution:
         for i,(v1,v2) in enumerate(edges):
             G[v1].append((v2,succProb[i]))
             G[v2].append((v1,succProb[i]))
-        maxHeap=[(-1,start_node)]
-        prob=[0]*n
+        maxHeap=[(-1.0,start_node)]
+        prob=[0.0]*n
         visited=[False]*n
 
         while maxHeap:
             cost_to_reach,node=heapq.heappop(maxHeap)
-            cost_to_reach*=-1
+            cost_to_reach*=-1.0
 
             if visited[node]:
                 continue
@@ -21,7 +21,7 @@ class Solution:
                 return cost_to_reach
             for nei,p in G[node]:
                 if not visited[nei]:
-                    new_prob=cost_to_reach*p*-1
+                    new_prob=cost_to_reach*p*-1.0
                     heapq.heappush(maxHeap,(new_prob,nei))
         # print(prob)
         return prob[end_node]
