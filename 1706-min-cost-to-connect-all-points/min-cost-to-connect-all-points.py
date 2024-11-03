e1277e8cbe144fa5b7ae2801dedf0bdccc1cd156
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-03 19:13:10 +0530

    Time: 2127 ms (36.01%) | Memory: 78.4 MB (85.78%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,12 +1,5 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        G=collections.defaultdict(list)
-
-        for x1,y1 in points:
-            for x2,y2 in points:
-                cost=abs(x1-x2)+abs(y1-y2)
-                G[(x1,y1)].append(((x2,y2),cost))
-        
         minHeap=[(0,0)]
         visited=[False]*(len(points))
         total=0
