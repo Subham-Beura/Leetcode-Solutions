d8d1f3d668664f0b9dbe1770c57d1144f54b5f4b
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-05 21:35:59 +0530

    Time: 747 ms (27.52%) | Memory: 225.8 MB (11.60%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,18 +1,15 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        cache={}
+        @cache
         def findDistinct(si,ti):
             if ti==len(t):
                 return 1
             if si==len(s):
                 return 0
-            if (si,ti) in cache:
-                return cache[(si,ti)]
             
             if s[si]==t[ti]:
-                cache[(si,ti)]=findDistinct(si+1,ti+1)+findDistinct(si+1,ti)
-            else:
-                cache[(si,ti)]=findDistinct(si+1,ti)
-            return cache[(si,ti)]
+                return findDistinct(si+1,ti+1)+findDistinct(si+1,ti)
+            return findDistinct(si+1,ti)
+
         return findDistinct(0,0)
                    
