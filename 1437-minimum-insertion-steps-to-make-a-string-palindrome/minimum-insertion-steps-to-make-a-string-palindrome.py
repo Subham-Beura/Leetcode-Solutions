72b790f06aa3b17572e99191f122309a17d275c8
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-05 20:47:43 +0530

    Time: 563 ms (59.88%) | Memory: 18.5 MB (67.74%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,20 +1,15 @@
-class Solution(object):
-    def minInsertions(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Find LCS Length
-        n=len(s)
-        dp=[[0]*(n+1) for _ in xrange(n+1)]
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        def lcs(s1,s2):
+            n=len(s1)
+            m=len(s2)
 
-        s2=s[::-1]
-        for i in xrange(n-1,-1,-1):
-            for j in xrange(n-1,-1,-1):
-                if s[i]==s2[j]:
-                    dp[i][j]=dp[i+1][j+1]+1
-                else:
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
-        return n-dp[0][0]
-
-        
+            dp=[[0]*(m+1) for _ in range(n+1)]
+            for r in range(n-1,-1,-1):
+                for c in range(m-1,-1,-1):
+                    if s1[r]==s2[c]:
+                        dp[r][c]=dp[r+1][c+1]+1
+                    else:
+                        dp[r][c]=max(dp[r+1][c],dp[r][c+1])
+            return dp[0][0] 
+        return len(s)-lcs(s,s[::-1])
