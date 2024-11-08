fbbe03e352ef8427c899d56dbffe7e32f4b0d233
Subham Beura <93039130+Subham-Beura@users.noreply.github.com>
2024-11-08 19:40:13 +0530

    Time: 1030 ms (12.60%) | Memory: 17 MB (54.65%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -3,27 +3,19 @@ class Solution:
         words.sort(key=lambda x: (len(x),x)) 
 
         n=len(words)
-        cache=[1]*n
+        cache={word:1 for word in words}
 
-        def isChain(smaller,bigger):
-            if smaller in bigger:
-                return True
-            for i,c in enumerate(bigger):
-                if bigger[:i]+bigger[i+1:] ==smaller:
-                    return True 
-            return False
         # print(words)
+        
         max_len=1
-        for i in range(n-2,-1,-1):
-            word_length=len(words[i])
-            for j in range(i+1,n):
-                if len(words[j])>word_length+1:
-                    break
-                if len(words[j])==word_length or not isChain(words[i],words[j]):
-                    continue
-                # print(f"{words[i]} : {words[j]}")
-                cache[i]=max(cache[i],cache[j]+1)
-            max_len=max(max_len,cache[i]) 
-        print(cache)
+        for word in words:
+            word_length=len(word)
+           
+            for i in range(word_length):
+                prev_word= word[:i]+word[i+1:] 
+                if prev_word in words:
+                    cache[word]=max(cache[word],cache[prev_word]+1)
+            max_len=max(max_len,cache[word]) 
+        # print(cache)
                 
         return max_len 
